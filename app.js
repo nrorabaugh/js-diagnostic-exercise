4e136aa3f41cdeaba37da0e410b4c2d8d4bd86e2
Noah Rorabaugh <noahrorabaugh@Noahs-MacBook-Pro.local>
2019-10-01 11:18:37 -0400

    Added deposit function

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 // Store accounts, inputs, balances, and buttons inside variables
-// Translated variables into accounts with properties
+// Translated variables into objects with properties by account
+// Write deposit functions as methods inside account objects 
+
 let savings = document.getElementById('savings')
 let checking = document.getElementById('checking')
+
 let save = {
     wdraw: savings.getElementsByClassName('withdraw')[0],
     depo: savings.getElementsByClassName('deposit')[0],
     balance: savings.getElementsByClassName('balanceNum')[0],
-    input: savings.getElementsByClassName('input')[0]
+    input: savings.getElementsByClassName('input')[0],
+    deposit: function() {
+        let amt = parseInt(save.input.value)
+        let bal = parseInt(save.balance.innerHTML)
+        save.balance.innerHTML = bal + amt
+    }
 }
+
 let check = {
     input: checking.getElementsByClassName('input')[0],
     wdraw: checking.getElementsByClassName('withdraw')[0],
     balance: checking.getElementsByClassName('balanceNum')[0],
-    depo: checking.getElementsByClassName('deposit')[0]
+    depo: checking.getElementsByClassName('deposit')[0],
+    deposit: function() {
+        let amt = parseInt(check.input.value)
+        let bal = parseInt(check.balance.innerHTML)
+        check.balance.innerHTML = bal + amt
+    }
 }
 
