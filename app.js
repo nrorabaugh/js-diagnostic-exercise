3c2ad5d3a613ea2cadf75f04337e9c1b6aea3e52
Noah Rorabaugh <noahrorabaugh@Noahs-MacBook-Pro.local>
2019-10-01 10:40:35 -0400

    Created variables

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello from app.js")
+// Store accounts, inputs, balances, and buttons inside variables
+let savings = document.getElementById('savings')
+let checking = document.getElementById('checking')
+let savingsDepo = savings.getElementsByClassName('deposit')[0]
+let checkingDepo = checking.getElementsByClassName('deposit')[0]
+let savingsInput = savings.getElementsByClassName('input')[0]
+let checkingInput = checking.getElementsByClassName('input')[0]
+let savingsWdraw = savings.getElementsByClassName('withdraw')[0]
+let checkingWdraw = checking.getElementsByClassName('withdraw')[0]
+let checkingBalance = checking.getElementsByClassName('balanceNum')[0]
+let savingsBalance = savings.getElementsByClassName('balanceNum')[0]
+
+
+
