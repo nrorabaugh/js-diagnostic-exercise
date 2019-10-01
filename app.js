2f5e3c489439254e0521ffc4d0fb3eacea26502b
Noah Rorabaugh <noahrorabaugh@Noahs-MacBook-Pro.local>
2019-10-01 10:54:51 -0400

    Translate to objects

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 // Store accounts, inputs, balances, and buttons inside variables
+// Translated variables into accounts with properties
 let savings = document.getElementById('savings')
 let checking = document.getElementById('checking')
-let savingsDepo = savings.getElementsByClassName('deposit')[0]
-let checkingDepo = checking.getElementsByClassName('deposit')[0]
-let savingsInput = savings.getElementsByClassName('input')[0]
-let checkingInput = checking.getElementsByClassName('input')[0]
-let savingsWdraw = savings.getElementsByClassName('withdraw')[0]
-let checkingWdraw = checking.getElementsByClassName('withdraw')[0]
-let checkingBalance = checking.getElementsByClassName('balanceNum')[0]
-let savingsBalance = savings.getElementsByClassName('balanceNum')[0]
-
-
+let save = {
+    wdraw: savings.getElementsByClassName('withdraw')[0],
+    depo: savings.getElementsByClassName('deposit')[0],
+    balance: savings.getElementsByClassName('balanceNum')[0],
+    input: savings.getElementsByClassName('input')[0]
+}
+let check = {
+    input: checking.getElementsByClassName('input')[0],
+    wdraw: checking.getElementsByClassName('withdraw')[0],
+    balance: checking.getElementsByClassName('balanceNum')[0],
+    depo: checking.getElementsByClassName('deposit')[0]
+}
 
